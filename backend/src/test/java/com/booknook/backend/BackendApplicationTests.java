59e8e618147da5e65e1179078028566846c71bb7
YuKi <yukifernando0001@gmail.com>
2024-12-31 13:01:26 +0530

    [new]: backend init

---
+++ backend/src/test/java/com/booknook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booknook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
